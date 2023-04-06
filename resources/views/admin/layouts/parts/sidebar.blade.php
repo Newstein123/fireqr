559b9484f5539737bf02c8963995950162a292b3
Htet Thinzar Moe <57472179+htetthinzarmoe123@users.noreply.github.com>
2023-04-06 15:50:52 +0630

    First commit

---
+++ resources/views/admin/layouts/parts/sidebar.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav metismenu" id="side-menu">
+            <li class="nav-header">
+                <div class="dropdown profile-element">
+                    <img alt="image" class="rounded-circle" src="img/profile_small.jpg"/>
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                        <span class="block m-t-xs font-bold">David Williams</span>
+                        <span class="text-muted text-xs block">Art Director <b class="caret"></b></span>
+                    </a>
+                    <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                        <li><a class="dropdown-item" href="profile.html">Profile</a></li>
+                        <li><a class="dropdown-item" href="contacts.html">Contacts</a></li>
+                        <li><a class="dropdown-item" href="mailbox.html">Mailbox</a></li>
+                        <li class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="login.html">Logout</a></li>
+                    </ul>
+                </div>
+                <div class="logo-element">
+                    IN+
+                </div>
+            </li>
+            <li class="active">
+                <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span></a>
+            </li>
+            <li>
+                <a href="layouts.html"><i class="fa fa-product-hunt" aria-hidden="true"></i> <span class="nav-label"> Product </span></a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-users"></i> <span class="nav-label"> Admin အသုံးပြုသူများ  </span></a>
+            </li>
+            <li>
+                <a href="mailbox.html"><i class="fa fa-cogs"></i> <span class="nav-label"> Setting  </span><span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level collapse">
+                    <li><a href="mailbox.html">Inbox</a></li>
+                    <li><a href="mail_detail.html">Email view</a></li>
+                    <li><a href="mail_compose.html">Compose email</a></li>
+                    <li><a href="email_template.html">Email templates</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
